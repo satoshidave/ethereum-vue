0128ece01f3248ed34faea103bb838b703fe72af
David Porras <dporras@iMac-de-David.local>
2018-05-15 18:49:30 -0400

    Deprecated Metamask detect

--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,12 @@
 import Web3 from 'web3'
 
-const web3 = new Web3(window.web3.currentProvider)
+var web3
+const web3Instance = new Web3(window.web3)
+if (web3Instance.currentProvider !== null) {
+  web3 = new Web3(window.web3.currentProvider)
+} else {
+  console.log('web3 no está instalado.')
+  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+}
 
 export default web3
