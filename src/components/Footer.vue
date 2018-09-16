f40feee146e438da44e3d5cfe00d10059a9c0a1b
David Porras <david.porras12@gmail.com>
2018-09-15 22:23:23 -0300

    Github link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,8 +4,8 @@
       <v-card-title class="teal white--text">
         <strong class="subheading">More in my social networks!</strong>
         <v-spacer></v-spacer>
-        <v-btn v-for="(icon, index) in icons" :key="index" icon class="mx-3">
-          <v-icon dark >{{icon}}</v-icon>
+        <v-btn v-for="(icon, index) in icons" :key="index" icon class="mx-3" @click="goTo(icon.link)" >
+          <v-icon dark >{{icon.icon}}</v-icon>
         </v-btn>
       </v-card-title>
     </v-card>
@@ -18,12 +18,17 @@ export default {
   data () {
     return {
       icons: [
-        'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-instagram',
-        'fab fa-github'
+        {name: 'Facebook', icon: 'fab fa-facebook', link: '#'},
+        {name: 'Twitter', icon: 'fab fa-twitter', link: '#'},
+        {name: 'Instagram', icon: 'fab fa-instagram', link: '#'},
+        {name: 'Facebook', icon: 'fab fa-github', link: 'https://github.com/daveporras'}
       ],
       mobile: null
+    }
+  },
+  methods: {
+    goTo (url) {
+      if (url !== '#') window.open(url, '_blank')
     }
   }
 }
